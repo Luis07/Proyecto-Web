da6c2fadc08d12f1618bc41f02f07455622a437f
Luis <luisbrenes250594@gmail.com>
2015-11-07 22:08:06 -0600

    index.js
    tiene el codigo js que nos permite guardar y validar la informacion que
    esta en el localstorage,, pero le falta un poco para que quede al 100%

---
+++ index.js
@@ -0,0 +1,71 @@
+var LOGIN=LOGIN||
+{
+
+	guardarUser: function() {
+
+		var usuario = document.getElementById("usuario").value;
+		var contrasena = document.getElementById("contrasena").value;
+
+
+		LOGIN.guardar(usuario,contrasena);
+
+
+	},
+
+	guardar:function(usuario,contrasena)
+	{
+		this.usuario=usuario;
+		this.contrasena=contrasena;
+
+       //lo obtiene todo en si
+       dato = localStorage.getItem("Login");
+
+       //lo obtiene en objetos 
+       dato = JSON.parse(localStorage.getItem("Login"));
+
+       if(dato==null)
+       {
+       	var datosLogin=new Array();
+
+       	var date={'User':this.usuario,'Password':this.contrasena};
+       	datosLogin.push(date);
+          //Este es para ser el key del localstorage y le envia un arreglo de objetos		
+          localStorage.setItem("Login",JSON.stringify(datosLogin));
+      }else
+      {
+
+      	var date={'User':this.usuario,'Password':this.contrasena};
+      	dato.push(date);
+        //Este es para ser el key del localstorage y le envia un arreglo de objetos		
+        localStorage.setItem("Login",JSON.stringify(dato));
+
+    }
+
+
+},validar:function(usuario,contrasena)
+{
+	this.usuario=usuario;
+	this.contrasena=contrasena;
+
+	dato = localStorage.getItem("Login");
+
+	dato = JSON.parse(localStorage.getItem("Login"));
+
+	for (var i = 0; i <dato.length; i++) {
+		/*validacion de los usuarios*/
+
+		if(dato[i].User==this.usuario&&dato[i].Password==this.contrasena){
+			window.open('http://localhost/Luis/correo.html','','_self').value;
+			/**alert(dato[i].User + dato[i].Password);**/
+		}
+	}
+},Login:function()
+
+{
+	var usuario = document.getElementById("usuario").value;
+	var contrasena = document.getElementById("contrasena").value;
+	LOGIN.validar(usuario,contrasena);
+}
+
+};
+
