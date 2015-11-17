3439d57a76321813cf685d833f34232f2d9da4fc
Luis <luisbrenes250594@gmail.com>
2015-11-17 17:07:22 -0600

    tiene la funcion para guardar borradores
    se pueden guardar borradores

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ var LOGIN=LOGIN||
 {
 
 	guardarUser: function() {
-		debugger;
 		var usuario = document.getElementById("usuarioregistro").value;
 		var contrasena = document.getElementById("contrasenaregistro").value;
-		LOGIN.validarEmail(usuario,contrasena);
+		LOGIN.validarEmailregistro(usuario,contrasena);
 	},
 	guardar:function(usuario,contrasena)
 	{
+		debugger;
 		this.usuario=usuario;
 		this.contrasena=contrasena;
        //lo obtiene todo en si
        dato = localStorage.getItem("Login");
        //lo obtiene en objetos 
        dato = JSON.parse(localStorage.getItem("Login"));
-       debugger;
        if(!LOGIN.correosiguales(this.usuario)){
-       if(dato==null)
-       {
-       	var datosLogin=new Array();
-       	var date={'User':this.usuario,'Password':this.contrasena};
-       	datosLogin.push(date);
+       	if(dato==null)
+       	{
+       		var datosLogin=new Array();
+       		var date={'User':this.usuario,'Password':this.contrasena};
+       		datosLogin.push(date);
           //Este es para ser el key del localstorage y le envia un arreglo de objetos		
           localStorage.setItem("Login",JSON.stringify(datosLogin));
-          LOGIN.crearbandejas();
           window.open("index.html");
           window.close("registrousuarios.html");
       }
@@ -34,24 +32,23 @@ var LOGIN=LOGIN||
       	dato.push(date);
         //Este es para ser el key del localstorage y le envia un arreglo de objetos		
         localStorage.setItem("Login",JSON.stringify(dato));
-        LOGIN.crearbandejas();
         window.open("index.html");
         window.close("registrousuarios.html");}
     }else{
 
-alert("El correo: "+usuario+" ya existe");
+    	alert("El correo: "+usuario+" ya existe");
     }
-    },validar:function(usuario,contrasena)
-    {
-    	this.usuario=usuario;
-    	this.contrasena=contrasena;
-    	dato = localStorage.getItem("Login");
-    	dato = JSON.parse(localStorage.getItem("Login"));
-    	for (var i = 0; i <dato.length; i++) {
-    		/*validacion de los usuarios*/
-    		if(dato[i].User===this.usuario&&dato[i].Password===this.contrasena)
-    		{
-    			var conectado={'conectado':this.usuario};
+},validar:function(usuario,contrasena)
+{
+	debugger;
+	this.usuario=usuario;
+	this.contrasena=contrasena;
+    var dato=JSON.parse(localStorage.getItem("Login"));
+	for (var i = 0; i <dato.length; i++) {
+		/*validacion de los usuarios*/
+		if(dato[i].User===this.usuario&&dato[i].Password===this.contrasena)
+		{
+			var conectado={'conectado':this.usuario};
         //Este es para ser el key del localstorage y le envia un arreglo de objetos		
         localStorage.setItem("Enlínea",JSON.stringify(conectado));
         window.open("correo.html");
@@ -59,45 +56,61 @@ alert("El correo: "+usuario+" ya existe");
 }
 },Login:function()
 {
+	debugger;
 	var usuario = document.getElementById("usuario").value;
 	var contrasena = document.getElementById("contrasena").value;
 	LOGIN.validar(usuario,contrasena);
 },validarEmail:function ( usuario,contrasena ) {
 	var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	if(!LOGIN.camposvacios())
-	{
+	if ( !expr.test(usuario) ){
+		alert("Error: La dirección de correo " + usuario + " es incorrecta.");
+		return false;
+	}
+	return true;
+},validarEmailregistro:function ( usuario,contrasena ) {
 
-		if ( !expr.test(usuario) ){
-			alert("Error: La dirección de correo " + usuario + " es incorrecta.");
-		}else{
+	var datosformulario=document.form_registro;
+	if(!LOGIN.camposvacios(datosformulario))
+	{
+		if(LOGIN.validarEmail(usuario,contrasena)){
 			LOGIN.guardar(usuario,contrasena);
 		}
+		
 	}	
-},camposvacios:function()
+},camposvacios:function(datosf)
 {
-	var datosformulario=document.form_registro;
+	var datosformulario=datosf;
 	for (var i=0; i<datosformulario.length; i++) {
-		if(datosformulario[i].type ==='text'||datosformulario[i].type ==='password'||datosformulario[i].type ==='month') {
-			if (datosformulario[i].value === null || datosformulario[i].value.length === 0 || /^\s*$/.test(datosformulario[i].value)){
-				alert (datosformulario[i].name+ ' no puede estar vacío');
-				return true;	}
+		if(datosformulario[i].type ==='text'||datosformulario[i].type ==='textarea')
+		{
+			if (datosformulario[i].value === null || datosformulario[i].value.length === 0)
+			{
+				alert("No se permiten campos vacios");
+				return true;
 			}
 		}
-	},imagen:function() {
-		files=document.getElementById('imagen').value;
-		if(LOGIN.comprueba_extension(files)){
-			var file = files;
-			var imageType = /image.*/;
-			var img = document.createElement("img");
-			img.classList.add("obj");
-			img.file = file;
-			debugger;
-			document.getElementById("foto").src =img.file;
+		if(datosformulario[i].type ==='text'||datosformulario[i].type ==='password'||datosformulario[i].type ==='month') {
+			if (datosformulario[i].value === null || datosformulario[i].value.length === 0 || /^\s*$/.test(datosformulario[i].value)){
+				alert ( ' no pueden haber campos vacios ');
+				return true;	
+			}
 		}
-	}, comprueba_extension:function(archivo) { 
-		debugger;	
-		extensiones_permitidas = new Array(".png", ".jpg"); 
-		mierror = ""; 
+	}
+},imagen:function() {
+	files=document.getElementById('imagen').value;
+	if(LOGIN.comprueba_extension(files)){
+		var file = files;
+		var imageType = /image.*/;
+		var img = document.createElement("img");
+		img.classList.add("obj");
+		img.file = file;
+		debugger;
+		document.getElementById("foto").src =img.file;
+	}
+}, comprueba_extension:function(archivo) { 
+	debugger;	
+	extensiones_permitidas = new Array(".png", ".jpg"); 
+	mierror = ""; 
        //recupero la extensión de este nombre de archivo 
        extension = (archivo.substring(archivo.lastIndexOf("."))).toLowerCase(); 
       //compruebo si la extensión está entre las permitidas 
@@ -129,49 +142,98 @@ alert("El correo: "+usuario+" ya existe");
 	$('#conectado').html(usuacon);	
 	
 },
-	crearbandejas: function() {
-		var usuario = document.getElementById("usuarioregistro").value;
-		LOGIN.crear(usuario);
-	},
-	crear:function(usuario)
-	{
-		debugger;
-		this.usuario=usuario;
-       //lo obtiene todo en si
+crearbandejas: function() {
+	var usuario = document.getElementById("usuarioregistro").value;
+	LOGIN.crear(usuario);
+},
+crear:function(usuario)
+{
+	debugger;
+	this.usuario=usuario;
+       //lo obtiene todo en si en un arreglo de datos
        bentrada = JSON.parse(localStorage.getItem("Bandejaentrada"));
        bsalida = JSON.parse(localStorage.getItem("Bandejasalida"));
        if(bsalida===null&&bentrada===null)
        {
-       	var datosenviados=new Array();
-       	var datosbsalida=new Array();
        	var enviados={'User':this.usuario,'para':"",'asunto':"",'contenido':""};
        	var salida={'User':this.usuario,'para':"",'asunto':"",'contenido':""};
-       	datosenviados.push(enviados);
-        datosbsalida.push(salida);
+       	bentrada.push(enviados);
+       	bsalida.push(salida);
           //Este es para ser el key del localstorage y le envia un arreglo de objetos		
-          localStorage.setItem("Bandejasalida",JSON.stringify(salida));
-          localStorage.setItem("Bandejaenviados",JSON.stringify(enviados));
+          localStorage.setItem("Bandejasalida",JSON.stringify(bsalida));
+          localStorage.setItem("Bandejaenviados",JSON.stringify(bentrada));
       }
       else
       {
-       	var enviados={'User':this.usuario,'para':"",'asunto':"",'contenido':""};
-       	var salida={'User':this.usuario,'para':"",'asunto':"",'contenido':""};
+      	var enviados={'User':this.usuario,'para':"",'asunto':"",'contenido':""};
+      	var salida={'User':this.usuario,'para':"",'asunto':"",'contenido':""};
       	bsalida.push(salida);
       	bentrada.push(enviados);
-        localStorage.setItem("Bandejasalida",JSON.stringify(salida));
-        localStorage.setItem("Bandejaenviados",JSON.stringify(enviados));
+      	localStorage.setItem("Bandejasalida",JSON.stringify(bsalida));
+      	localStorage.setItem("Bandejaenviados",JSON.stringify(bentrada));
       }
-},correosiguales:function(usuario)
-{
-	debugger;
-	this.usuarionuevo=usuario;
-  dato = JSON.parse(localStorage.getItem("Login"));
-     for (var i = 0; i < dato.length; i++) {
-     	if(dato[i].User===this.usuarionuevo)
-     	{
-return true;
-     	}
-     }
-}
-};
+  },correosiguales:function(usuario)
+  {
+  	this.usuarionuevo=usuario;
+  	dato = JSON.parse(localStorage.getItem("Login"));
+  	if(dato==null){
+  		return false;
+  	}else{
+  		for (var i = 0; i < dato.length; i++) {
+  			if(dato[i].User===this.usuarionuevo)
+  			{
+  				return true;
+  			}
+  		}
+  	}
+  },bandejaenviados:function()
+  {
+  	LOGIN.cambiarestilo();
+  },cambiarestilo:function()
+  {
+  	var elemento = document.getElementById("bs");
+  	var cambio=document.getElementById("be");
+  	elemento.className = "btn btn-primary bajarunpoco grande ";
+  	cambio.className="btn btn-primary bajarunpoco grande color marcado";
+  	$('bs').html(elemento);
+  	$('be').html(cambio);
 
+  },bandejasalida:function()
+  {
+  	LOGIN.cambiarestilob();
+  },cambiarestilob:function()
+  {
+  	var elemento = document.getElementById("bs");
+  	var cambio=document.getElementById("be");
+  	elemento.className = "btn btn-primary bajarunpoco grande color marcado";
+  	cambio.className="btn btn-primary bajarunpoco grande";
+  	$('bs').html(elemento);
+  	$('be').html(cambio);
+
+  }
+
+  ,guardarbandejasalida:function()
+  {
+  	debugger;
+  	var para=document.getElementById("mensajepara").value;
+  	var asunto=document.getElementById("asuntomensaje").value;
+  	var contenido=document.getElementsByClassName("contenidomensaje").value;
+  	if(LOGIN.validarEmail(para)){
+  		var datosformulario=document.formredactar;
+  		if(!LOGIN.camposvacios(datosformulario))
+  		{
+  			var bandeja=new Array();
+  			var band=JSON.parse(localStorage.getItem("Bandejasalida"));
+  	    var nuevo={'User':usuacon,'para':para,'asunto':asunto,'contenido':contenido};
+if(band===null){
+  bandeja.push(nuevo);
+  localStorage.setItem("Bandejasalida",JSON.stringify(bandeja));
+}else{
+  band.push(nuevo);  
+  localStorage.setItem("Bandejasalida",JSON.stringify(band));
+}
+  			}
+  		}
+  	}
+
+};
